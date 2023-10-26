e2d3ba657987f4f97baa21da889d8072602ad245
loureir <80390415+carolinaloureir@users.noreply.github.com>
2023-10-26 12:40:18 +0100

    baguette

--- Assig1/data/transformData.py
+++ Assig1/data/transformData.py
@@ -3,8 +3,12 @@ from math import floor
 path = "C:\\Users\\diogo\\Desktop\\Mestrado\Masters\\Masters-MEI\\Assig1\\data\\";
 dayInSecs = 86400;
 
-
+daysOfWeek = ["Tuesday","Wednesday", "Thursday", "Friday", "Saturday","Sunday","Monday"];
+months = ["Oct", "Nov", "Dec", "Jan", "Feb", "Mar","Apr", "May", "Jun", "Jul", "Aug", "Sep"];
+years = [1994, 1995, 1996];
 fileOutput = open(path + "dataWithDates.csv", "w");
+
+
 
 
 
@@ -23,18 +27,24 @@ with open(path+"data.csv", "r") as csvfileInput:
     csvreader = csv.reader(csvfileInput);
     header = next(csvreader)[0];
     headerSplitted = header.split(";");
-    headerSplitted[1] = 'Day Submited';
+    headerSplitted.append('Hour of Day');
+    headerSplitted.append('Day');
+    headerSplitted.append('Day of week');
+    headerSplitted.append('Week');
+    headerSplitted.append('Month');
+    headerSplitted.append('Year');
 
+    
     fileOutput.write(format(headerSplitted));
     
         
-    for row in csvreader:
-        newRow = row[0].split(";");
+    # for row in csvreader:
+    #     newRow = row[0].split(";");
         
-        valueChanged = floor(int(newRow[1])/dayInSecs);
-        newRow[1] = valueChanged;
+    #     valueChanged = floor(int(newRow[1])/dayInSecs);
+    #     newRow[1] = valueChanged;
         
-        fileOutput.write(format(newRow));
+    #     fileOutput.write(format(newRow));
     
     
     fileOutput.close();
