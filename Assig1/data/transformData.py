3274e7a89deed3f3d0b0e78a208b460486998799
Diogo Rosário <41454360+SaltyChef@users.noreply.github.com>
2023-10-26 23:35:18 +0100

    final

--- Assig1/data/transformData.py
+++ Assig1/data/transformData.py
@@ -1,13 +1,42 @@
 import csv
-from math import floor
+import datetime
 path = "C:\\Users\\diogo\\Desktop\\Mestrado\Masters\\Masters-MEI\\Assig1\\data\\";
-dayInSecs = 86400;
 
-daysOfWeek = ["Tuesday","Wednesday", "Thursday", "Friday", "Saturday","Sunday","Monday"];
-months = ["Oct", "Nov", "Dec", "Jan", "Feb", "Mar","Apr", "May", "Jun", "Jul", "Aug", "Sep"];
-years = [1994, 1995, 1996];
+
+dayInSecs = 86400;
+hourInSecs = 3600;
+normalYearInSec = 31536000;
+
+year = 1994;
+startTime = (276 * 3600 * 24) + 25272; 
+
+
 fileOutput = open(path + "dataWithDates.csv", "w");
 
+
+def get_day_month_day_hour(seconds, year):
+    # Convert seconds to a datetime object
+    dt = datetime.datetime.fromtimestamp(seconds)
+
+    # Set the year of the datetime object
+    dt = dt.replace(year=year)
+
+    # Subtract 1 hour from the datetime
+    dt = dt - datetime.timedelta(hours=1)
+
+    # Get the day of the week (0=Monday, 6=Sunday)
+    day_of_week = dt.strftime('%A')
+
+    # Get the month
+    month = dt.strftime('%B')
+
+    # Get the day of the month
+    day_of_month = dt.day
+
+    # Get the hour of the day
+    hour_of_day = dt.strftime('%H')
+
+    return day_of_week, month, day_of_month, hour_of_day
 
 
 
@@ -18,7 +47,6 @@ def format(row):
         line += str(text) + ";";
     line = line.removesuffix(";")
     return line + "\n";
-            
 
 
 
@@ -27,26 +55,40 @@ with open(path+"data.csv", "r") as csvfileInput:
     csvreader = csv.reader(csvfileInput);
     header = next(csvreader)[0];
     headerSplitted = header.split(";");
-    headerSplitted.append('Hour of Day');
+    headerSplitted.append('Hour');
     headerSplitted.append('Day');
-    headerSplitted.append('Day of week');
-    headerSplitted.append('Week');
+    headerSplitted.append('Day of the week');
     headerSplitted.append('Month');
     headerSplitted.append('Year');
 
     
     fileOutput.write(format(headerSplitted));
     
-        
-    # for row in csvreader:
-    #     newRow = row[0].split(";");
-        
-    #     valueChanged = floor(int(newRow[1])/dayInSecs);
-    #     newRow[1] = valueChanged;
-        
-    #     fileOutput.write(format(newRow));
+    currentTime = startTime;   
     
+    for row in csvreader:
+        
+        newRow = row[0].split(";");
+      
+        currentTime = (startTime + int(newRow[1])) ;
     
+        if(currentTime < normalYearInSec):  
+            year = 1994
+        elif currentTime >= normalYearInSec and currentTime < 2*normalYearInSec :
+            year = 1995    
+        elif currentTime >= 2*normalYearInSec and currentTime < (2*normalYearInSec + startTime):
+            year = 1996
+        
+        day_of_the_week, month, day_of_month, hour = get_day_month_day_hour(currentTime, year)
+        
+        newRow.append(hour);
+        newRow.append(day_of_month);
+        newRow.append(day_of_the_week);
+        newRow.append(month);    
+        newRow.append(year);    
+        fileOutput.write(format(newRow));
+
+
     fileOutput.close();
     csvfileInput.close();
     
